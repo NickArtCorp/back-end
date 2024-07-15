20c3d0d4463910b807a8ddc2b5091ff01ddfe881
NickArtCorp <ncorp237@gmail.com>
2024-07-15 05:44:23 +0200

    yes

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('', include('restaurant.urls'))
-]
+   path('', include('restaurant.urls')),
+   path('restaurant/menu/', include('restaurant.urls')),
+   ]
