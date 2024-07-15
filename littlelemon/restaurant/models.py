0654612f30dad3636a27e5dc57492515c9af1cf0
NickArtCorp <ncorp237@gmail.com>
2024-07-15 03:47:07 +0200

    yes

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    menuID = models.IntegerField()
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    customer_id = models.IntegerField()
+    customer_name = models.CharField(max_length=255)
+    BookingDate = models.DateField()
+    number_of_guests = models.PositiveIntegerField()
+   
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.BookingDate}"
+
