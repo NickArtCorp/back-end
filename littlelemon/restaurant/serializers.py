784622001fc156ea421ec97d3e3a7c28a7c0b65f
NickArtCorp <ncorp237@gmail.com>
2024-07-18 20:44:17 +0200

    yes

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Menu
 
-class MenuSerializer(serializers.ModelSerializers):
+class MenuSerializer(serializers.ModelSerializer):
     class ModelSerializer:
       model = Menu
       fields = '__all__'
