dc0eefd174a856a3f2e5461ba858a403ddd7a8d4
NickArtCorp <ncorp237@gmail.com>
2024-07-20 21:51:20 +0200

    yes

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Menu
+from .models import Booking
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
 
 class MenuSerializer(serializers.ModelSerializer):
     class ModelSerializer:
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,12 +3,16 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from .models import Menu
 from .serializers import MenuSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
 
-# Create your views here. 
+# Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
  queryset = Menu.objects.all()
  serializer_class = MenuSerializer
@@ -16,3 +20,7 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
  queryset = Menu.objects.all()
  serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
